f83e85fd46d0a38ab9b6aa6320daf832bca9c694
marchena290 <marchena452@gmail.com>
2025-05-23 17:42:28 -0600

    Trabajando en la logic del proyecto, con un avance en general de mas de 50%

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -8,7 +8,6 @@ public class Principal {
     public static void main(String[] args) throws IOException, InterruptedException {
         ServicioConversion servicio = new ServicioConversion();
         servicio.obtenerTasasCambio();
-        servicio.obtenerMonedaCrc();
-        System.out.println("El valor de la moneda de costa rica es de: " + servicio.obtenerMonedaCrc());
+
     }
 }
